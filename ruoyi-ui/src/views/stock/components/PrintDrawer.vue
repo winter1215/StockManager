71377a4da4f4cf857a3da9ea0f194f0e721cd0ff
winter <2436197699@qq.com>
2023-07-07 16:11:09 +0800

    refactor: 需求重构

--- ruoyi-ui/src/views/stock/components/PrintDrawer.vue
+++ ruoyi-ui/src/views/stock/components/PrintDrawer.vue
@@ -10,6 +10,23 @@
             <div id="preview_content_custom"></div>
         </div>
 
+        <el-dialog title="出货面板" :visible.sync="dialogFormVisible" width="500px" append-to-body>
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="客户名称" prop="name">
+                    <el-input v-model.number="form.name" placeholder="请输入客户名称" />
+                </el-form-item>
+                <el-form-item label="价格" prop="price">
+                    <el-input v-model="form.price" placeholder="请输入出货价格" />
+                </el-form-item>
+                <el-form-item label="重量" prop="weight">
+                    <el-input v-model="form.weight" placeholder="请输入出货总重" />
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="doprint">确 定</el-button>
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
   
@@ -28,9 +45,12 @@ export default {
     },
     data() {
         return {
+            dialogFormVisible: false,
             waitShowPrinter: false,
+            formLabelWidth: '120px',
             // 纸张宽 mm
             width: 0,
+            form: {},
             // 模板
             hiprintTemplate: {},
             // 数据
@@ -38,7 +58,7 @@ export default {
                 zhi: 0,
                 ge: 0,
                 totalWeight: 0.0,
-                totalPrice: 999999,
+                totalPrice: 0,
                 totalCap: ""
             }
         }
@@ -62,7 +82,7 @@ export default {
             this.initData();
             this.show(hiprintTemplate);
         },
-        deep:true
+        deep: true
     },
     methods: {
         init() {
@@ -70,33 +90,27 @@ export default {
                 template: template
             });
         },
-        initData(param) {
+        initData() {
             // 拷贝 防止数据出错
-            if (param) {
-                this.printData.name = param.name;
-                this.printData.orderNum = param.orderNum;
-            }
 
             this.printData.makeDate = this.parseTime(new Date(), '{y}年{m}月{d}日')
             let tmp = [...this.dataProp] //浅拷贝，可能会修改上游数据
             tmp.forEach(item => {
-                item.totalPrice = item.weight * item.price; // 单件的总重
-                item.totalPrice = parseFloat(item.totalPrice.toFixed(2));
+                // item.totalPrice = item.weight * item.price; // 单件的总重
+                // item.totalPrice = parseFloat(item.totalPrice.toFixed(2));
+                // this.printData.totalPrice = this.printData.totalPrice + item.totalPrice; // 出货单的总重
 
-                this.printData.totalPrice = this.printData.totalPrice + item.totalPrice; // 出货单的总重
-
-
-                if (item.materialType == 0) {
+                if (item.materialType === 0 || item.materialType === '支') {
                     this.printData.zhi = this.printData.zhi + item.quantity;
                 } else {
                     this.printData.ge = this.printData.ge + item.quantity;
                 }
-                    item.materialType = item.materialType === 0 || item.materialType === '支' ? '支' : '件';
-                this.printData.totalWeight = this.printData.totalWeight + item.weight;
+                item.materialType = item.materialType === 0 || item.materialType === '支' ? '支' : '件';
+                // this.printData.totalWeight = this.printData.totalWeight + item.weight;
             })
             // 打印预览将 总价与总重保留两位小数
-            this.printData.totalPrice = parseFloat(this.printData.totalPrice.toFixed(2));
-            this.printData.totalWeight = parseFloat(this.printData.totalWeight.toFixed(2));
+            // this.printData.totalPrice = parseFloat(this.printData.totalPrice.toFixed(2));
+            // this.printData.totalWeight = parseFloat(this.printData.totalWeight.toFixed(2));
 
             this.printData.totalCap = this.toChinese(this.printData.totalPrice);
             this.printData.table = tmp;
@@ -112,52 +126,91 @@ export default {
                 this.spinning = false
             }, 500)
         },
-        
+
+
         print() {
-            this.$prompt('请输入客户姓名', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                inputErrorMessage: '请输入客户姓名'
-            }).then(async ({ value }) => {
-                const res = await getOrder();
-                let param = {};
-                param.name = value;
-                // 不是 data
-                param.orderNum = res.msg;
-                console.log(res);
+            this.dialogFormVisible = true;
+            this.form = {};
+            // this.$prompt('请输入客户姓名', '提示', {
+            //     confirmButtonText: '确定',
+            //     cancelButtonText: '取消',
+            //     inputErrorMessage: '请输入客户姓名'
+            // }).then(async ({ value }) => {
 
-                // 初始化数据
-                this.printData.zhi = 0;
-                this.printData.ge = 0;
-                this.printData.totalWeight = 0.0;
-                this.printData.totalPrice = 0.0;
-                this.printData.totalCap = "";
-                this.initData(param);
-                this.waitShowPrinter = true
-                this.hiprintTemplate.print(this.printData, {}, {
-                    callback: () => {
-                        this.waitShowPrinter = false
-                    }
-                })
+            //     const res = await getOrder();
+            //     let param = {};
+            //     param.name = value;
+            //     // 不是 data
+            //     param.orderNum = res.msg;
+            //     console.log(res);
 
-                let data = {};
-                data.stockOutInfoList = [...this.dataProp].map(item => {
-                    return {
-                        profileCode: item.profileCode,
-                        changeQuantity: item.quantity,
-                        changeWeight: item.weight,
-                        changePrice: item.price
-                    }
-                });
-                data.state = 1; // 待确定状态
-                const res1 = outStock(data);
-            }).catch(() => {
-                this.$message({
-                    type: 'info',
-                    message: '取消输入'
-                });
-            });
+            //     // 初始化数据
+            //     this.printData.zhi = 0;
+            //     this.printData.ge = 0;
+            //     this.printData.totalWeight = 0.0;
+            //     this.printData.totalPrice = 0.0;
+            //     this.printData.totalCap = "";
+            //     this.initData(param);
+            //     this.waitShowPrinter = true
+            //     this.hiprintTemplate.print(this.printData, {}, {
+            //         callback: () => {
+            //             this.waitShowPrinter = false
+            //         }
+            //     })
+
+            //     let data = {};
+            //     data.stockOutInfoList = [...this.dataProp].map(item => {
+            //         return {
+            //             profileCode: item.profileCode,
+            //             changeQuantity: item.quantity,
+            //             changeWeight: item.weight,
+            //             changePrice: item.price
+            //         }
+            //     });
+            //     data.state = 1; // 待确定状态
+            //     const res1 = outStock(data);
+            // }).catch(() => {
+            //     this.$message({
+            //         type: 'info',
+            //         message: '取消输入'
+            //     });
+            // });
         },
+
+        async doprint() {
+            const res = await getOrder();
+            // 不是 data
+            this.printData.name = this.form.name;
+            this.printData.totalWeight = this.form.weight;
+            this.printData.totalPrice = parseFloat((this.form.weight * this.form.price).toFixed(2));
+            this.printData.orderNum = res.msg;
+
+            // 初始化数据
+            this.printData.zhi = 0;
+            this.printData.ge = 0;
+            this.printData.totalCap = "";
+            this.printData.name 
+            this.initData();
+            this.waitShowPrinter = true
+            this.hiprintTemplate.print(this.printData, {}, {
+                callback: () => {
+                    this.waitShowPrinter = false
+                }
+            })
+
+            let data = {};
+            data.stockOutInfoList = [...this.dataProp].map(item => {
+                return {
+                    profileCode: item.profileCode,
+                    changeQuantity: item.quantity,
+                    changeWeight: item.weight,
+                    changePrice: item.price
+                }
+            });
+            data.state = 1; // 待确定状态
+            const res1 = outStock(data);
+        },
+
         toPdf() {
             this.hiprintTemplate.toPdf(this.printData, '打印预览pdf');
         },
